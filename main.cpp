512e5f88d958d7848904c5feab6acfd2b01ddf59
hahafunnyuserna <madeforschool69@gmail.com>
2024-10-15 18:50:38 -0500

    update
    all add-ons complete (kind of)
    (third add-on uses conversions to string instead)

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main(void)
     string quest = "Please enter your age:\n";
     string pass = "You're able to run for president!\n";
     string fail = "You're too young for the oval office.\n";
+    string error = "";
     int minimum = 35;
 
-    prompt (minimum, quest, pass, fail);
+    prompt (minimum, quest, pass, fail, error);
 }
--- prompt.hpp
+++ prompt.hpp
@@ -1,21 +1,52 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <stdlib.h>
+#include <cctype>
 
 using namespace std;
 
-void prompt (int min, string prompt, string valid, string invalid)
+void prompt (int min, string prompt, string valid, string invalid, string error)
 {
+    int minimum = min;
+    string input;
     bool isValid = false;
-    int temp;
+    int temp = 8675309;
     while (isValid == false)
     {
         cout << prompt;
-        cin >> temp;
-        if (temp >= min)
+        getline(cin, input);
+        transform(input.begin(), input.end(), input.begin(), ::toupper);
+        cout << input << endl;
+
+        if (input == "EXIT")
         {
-            cout << valid << endl;
-            isValid = true;
-        } else {
-            cout << invalid << endl;
+            cout << "Goodbye.\n";
+            break;
+        } else if (input == "DEFAULT") {
+            temp = 55;
+            cout << "Your age is " << temp << ".\n";
+        }
+        else if (std::any_of(input.begin(), input.end(), ::isdigit) == true)
+        {
+            temp = std::stoi(input);
+            if (temp <= 0)
+            {
+                cout << "ERROR: Impossible age.\n";
+            }
+            else {
+                cout << "Your age is " << temp << ".\n";
+                if (temp >= minimum)
+                {
+                    cout << valid;
+                    isValid = true;
+                } else {
+                    cout << invalid;
+                }
+            }
+        }
+        else {
+            cout << "ERROR: Not an actual number.\n";
         }
     }
 }
